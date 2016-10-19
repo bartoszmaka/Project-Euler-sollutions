18941407a09534383ec09892f88fe62e8d7f8f8b
bartoszmaka <moncznybartosz@gmail.com>
2016-10-19 21:53:06 +0200

    added c# solutions

---
+++ problem2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int SumOfFibonaccisUnderFourMillion()
+        {
+            int a = 0;
+            int b = 1;
+            int tmp = 0;
+            int sum = 0;
+            while (b <= 4000000)
+            {
+                tmp = a;
+                a = b;
+                b += tmp;
+                if (b % 2 == 0) { sum += b; }
+            }
+            return sum; 
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfFibonaccisUnderFourMillion());
+            Console.Read();            
+        }
+    }
+}
---
+++ problem3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    class Program
+    {
+        static int[] PrimeFactors(long number)
+        {
+            List<int> factors = new List<int>();
+            while (number != 1)
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        factors.Add(i);
+                        number /= i;
+                    }
+                }
+            }
+            // First factor is always going to be 1
+            factors.Remove(factors[0]);
+            return factors.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] factors = PrimeFactors(600851475143);
+            Console.WriteLine(factors.Max());
+            Console.Read();
+        }
+    }
+}
